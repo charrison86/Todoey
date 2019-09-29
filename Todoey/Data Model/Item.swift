b9382a434140b53d8ae72e91fbbedb5bc42a15b6
Chase <harrisonca86@gmail.com>
2019-09-29 16:56:54 -0500

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Chase Harrison on 9/29/19.
+//  Copyright © 2019 Chase Harrison. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var Done: Bool = false
+    
+    init() {
+        
+    }
+}
